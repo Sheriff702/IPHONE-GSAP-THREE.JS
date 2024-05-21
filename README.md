49ea48598404d7b021ededc3dc02735e033aa93f
Ahmed Sharif <144018452+Sheriff702@users.noreply.github.com>
2024-05-21 23:34:42 +0200

    Copied Apple Iphone 15Pro page

---
+++ README.md
@@ -0,0 +1,20 @@
+# React + Vite
+
+To Start the Project.
+
+## Step 1
+
+Npm install
+
+## Step 2
+
+Npm run dev
+
+Used on this Project
+
+###########
+REACT
+JAVASCRIPT
+GSAP
+THREE.JS
+###########
---
+++ src/main.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+
+//...
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://c2f34e8fe45358b2aec50d61c271b9b9@o4507293672800256.ingest.de.sentry.io/4507293675159632",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
